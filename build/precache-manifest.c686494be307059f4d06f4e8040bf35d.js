5fdec1a6e1585d0b7f4e0be6f8e8a34b09eb451a
SimoneFu04 <82705165+SimoneFu04@users.noreply.github.com>
2021-09-05 11:33:58 +0200

    Add files via upload

---
+++ build/precache-manifest.c686494be307059f4d06f4e8040bf35d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9621ed2c24e52f03bb72a1a62ff17d13",
+    "url": "/static/media/discordLogo.9621ed2c.png"
+  },
+  {
+    "revision": "8322efe497434e91f6ee1548a7520d84",
+    "url": "/static/media/cardsingle.8322efe4.png"
+  },
+  {
+    "revision": "be3b7c5e62c277523539a810de328706",
+    "url": "/static/media/1b.be3b7c5e.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "469e935c062b85aa74d0",
+    "url": "/static/js/main.469e935c.chunk.js"
+  },
+  {
+    "revision": "d6eb326ff75b426c8ab2",
+    "url": "/static/js/1.d6eb326f.chunk.js"
+  },
+  {
+    "revision": "469e935c062b85aa74d0",
+    "url": "/static/css/main.49dcc2ce.chunk.css"
+  },
+  {
+    "revision": "c3c1f25c9d867db383399ea0e6db07b2",
+    "url": "/index.html"
+  }
+];
